b80cee51dc179259d52b88a106e2efd6a109bdc3
jlsajfj <jlsajfj@hotmail.com>
2021-03-16 12:03:32 -0400

    starting program

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stock-chart
+
+Simple app to generate a chart from a ticker. I just want it for [Mental Bot](https://github.com/jlsajfj/MentalBot).
---
+++ stock-chart.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.9
+import sys
+import datetime
+import yfinance as yf
+
+def price(ticker: str, _period: str = 'max'):
+    stock = yf.Ticker(ticker)
+    hist = stock.history(period=_period, debug=False)
+    if hist.empty:
+        raise Exception('No ticker info')
+    print(hist)
+
+def main():
+    price(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
