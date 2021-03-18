32403ed277c6ad19bed53f4fb452c98d7a523198
jlsajfj <jlsajfj@hotmail.com>
2021-03-17 20:33:22 -0400

    added basic chart

--- stock-chart.py
+++ stock-chart.py
@@ -2,13 +2,17 @@
 import sys
 import datetime
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 def price(ticker: str, _period: str = 'max'):
     stock = yf.Ticker(ticker)
     hist = stock.history(period=_period, debug=False)
     if hist.empty:
         raise Exception('No ticker info')
-    print(hist[['Open','Close']])
+    openCloseData = hist[['Open', 'Close']]
+    print(openCloseData)
+    plt.plot(openCloseData)
+    plt.show()
 
 def main():
     price(sys.argv[1])
